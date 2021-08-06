7d00c467ca2794e5b6f13aa9bf268456c87cc1e5
Nader Nasr <nadernasr7@gmail.com>
2021-08-06 18:03:41 -0400

    debugging

--- next.config.js
+++ next.config.js
@@ -3,9 +3,5 @@ module.exports = {
   images: {
     domains: ['bit.ly', 'fakestoreapi.com']
   },
-  eslint: {
-    // Warning: Dangerously allow production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+  
 }
